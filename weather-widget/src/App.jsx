e1ad57fe6a92cc06b423ace2d79c18113d238af4
Jayesh Hadiyal <jayeshhadiyal2007@gmail.com>
2025-04-22 17:12:33 +0530

    rendering the weatherApp component

--- weather-widget/src/App.jsx
+++ weather-widget/src/App.jsx
@@ -1,18 +1,9 @@
-import Button from "@mui/material/Button";
-import DeleteIcon from '@mui/icons-material/Delete';
-import Alert from '@mui/material/Alert';
+import WeatherApp from "./WeatherApp.jsx";
 
 function App() {
-  function buttonclk() {
-    console.log("Button was clicked");
-  }
   return (
     <>
-      <h1>Demo of Material UI</h1>
-      <Button variant="contained" onClick={buttonclk} startIcon={<DeleteIcon />}>
-        delete
-      </Button>
-      <Alert severity="info">Delete option is given.</Alert>
+      <WeatherApp />
     </>
   );
 }
